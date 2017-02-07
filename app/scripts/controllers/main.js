1b915b7377bdcb83daaac7aaacb1d85dd7b95a3c
Luke Vander Poel <airscout99@gmail.com>
2017-02-06 21:58:23 -0800

    started api integration

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,15 @@
  * Controller of the capstone2017App
  */
 angular.module('capstone2017App')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope, events) {
+    $scope.events = events.query();
+
+    $scope.refreshCurrent = function(){
+        $scope.events = events.query({
+            location: $scope.location
+        });
+    };
+
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
--- app/scripts/services/events.js
+++ app/scripts/services/events.js
@@ -8,16 +8,18 @@
  * Factory in the capstone2017App.
  */
 angular.module('capstone2017App')
-  .factory('events', function () {
+  .factory('events', function ($resource) {
     // Service logic
     // ...
 
-    var meaningOfLife = 42;
-
     // Public API here
-    return {
-      someMethod: function () {
-        return meaningOfLife;
+    return $resource('http://api.eventful.com/rest/events/search?q=music&l=:location&within=:distance&units=miles&t=:time&c=music&app_key=mhNWqRwCNqmxbkrL',{}, {
+      query: {
+        method:'GET',
+        params:{
+          location: 'Seattle,us'
+        },
+        isArray:false
       }
-    };
+    });
   });
