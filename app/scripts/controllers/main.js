908981d48e1b1cdef5dad631c50ffe528c64d244
Luke Vander Poel <airscout99@gmail.com>
2017-02-08 20:39:07 -0800

    started daterangepicker. Partially working

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,9 +7,19 @@
  * # MainCtrl
  * Controller of the capstone2017App
  */
+// , ['daterangepicker']
+// , moment
+
+
 angular.module('capstone2017App')
-  .controller('MainCtrl', function ($scope, events) {
+
+  .controller('MainCtrl', function ($scope, events, moment) {
     $scope.events = events.query();
+
+    $scope.datePicker = {
+      startDate: null,
+      endDate: null,
+    };
 
     $scope.refreshCurrent = function(){
         $scope.events = events.query({
