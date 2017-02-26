1281526cb7fcaea61f5c4f36e1afb8c415e36f9b
Luke Vander Poel <airscout99@gmail.com>
2017-02-26 14:56:59 -0800

    changed jsonp query correct format

--- app/scripts/services/events.js
+++ app/scripts/services/events.js
@@ -9,12 +9,14 @@
  */
 angular.module('capstone2017App')
   .factory('events', function ($http) {
-    var eventAPI='http://api.eventful.com/json/events/search?app_key=mhNWqRwCNqmxbkrL&q=music';
+    var eventAPI='http://api.eventful.com/json/events/search';
     return {
       query: function(){
         return $http.jsonp(eventAPI, {
           jsonpCallbackParam: 'callback',
           params: {
+            q: 'music',
+            'app_key': 'mhNWqRwCNqmxbkrL',
             location: 'Seattle',
             within: '10',
             time: 'This Month',
