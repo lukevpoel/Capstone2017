329fb115e020fb8eef9590328552107d7090061b
Luke Vander Poel <airscout99@gmail.com>
2017-03-06 18:10:15 -0800

    verified working incoming data on indevents. Correct access in view

--- app/scripts/controllers/events.js
+++ app/scripts/controllers/events.js
@@ -9,17 +9,12 @@
  */
 angular.module('capstone2017App')
   .controller('EventsCtrl', function ($scope, $route, $routeParams, $location, indevents) {
-    $scope.numbers = [
-      '5',
-      '10',
-      '20',
-      '50',
-      '100'
-    ];
 
     $scope.eventID = $routeParams.eventID;
     $scope.indevents = indevents.query ({
       id: $scope.eventID
+    }).then(function(data){
+      $scope.eventdata = data.data;
     });
 
 
