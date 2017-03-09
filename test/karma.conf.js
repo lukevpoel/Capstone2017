216ef6679730678cb00dae9e5ef7a11517f6356c
Luke Vander Poel <airscout99@gmail.com>
2017-03-08 18:21:57 -0800

    added ui.bootstrap

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -32,6 +32,7 @@ module.exports = function(config) {
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/ngmap/build/scripts/ng-map.js',
+      'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'bower_components/moment/moment.js',
       'bower_components/bootstrap-daterangepicker/daterangepicker.js',
