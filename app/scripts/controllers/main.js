9fa5d32202684e9d80ab4b00bf83f5e23e9490ae
Luke Vander Poel <airscout99@gmail.com>
2017-03-12 19:04:58 -0700

    applied date filters, and truncate filter to main.js

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -68,4 +68,34 @@ angular.module('capstone2017App')
       'AngularJS',
       'Karma'
     ];
+  })
+  .filter('jsDate', function () {
+    return function (x) {
+        return new Date(x);
+    };
+  })
+  .filter('crop', function(){
+  	return function(input, limit, respectWordBoundaries, suffix){
+  		if (input === null || input === undefined || limit === null || limit === undefined || limit === '') {
+  			return input;
+  		}
+  		if (angular.isUndefined(respectWordBoundaries)) {
+  			respectWordBoundaries = true;
+  		}
+  		if (angular.isUndefined(suffix)) {
+  			suffix = '...';
+  		}
+
+  		if (input.length <= limit) {
+  			return input;
+  		}
+
+  		limit = limit - suffix.length;
+
+  		var trimmedString = input.substr(0, limit);
+  		if (respectWordBoundaries) {
+  			return trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(' '))) + suffix;
+  		}
+  		return trimmedString + suffix;
+    };
   });
